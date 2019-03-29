b29ab82acc5547b099794dd805ae5232a5ed059e
gaurav <gauravusadadiya1998@gmail.com>
2019-03-29 21:10:39 +0530

    Complete Multi Class FFNetwork

--- 7-FeedForwardNetwork/FeedForwardNeuralNetwork.py
+++ 7-FeedForwardNetwork/FeedForwardNeuralNetwork.py
@@ -394,12 +394,145 @@ plt.scatter(X_train[:, 0], X_train[:, 1], c = Y_pred_binarised_train, s = 15*(np
 
             
             
+""" -------------------------- Multy Class Classification of FFNetwork --------------------------"""
+
+# Generate Data
+        
+data, labels = make_blobs(n_samples = 1000, centers = 4, n_features = 2, random_state = 0)
+print(data.shape, labels.shape)
+
+plt.scatter(data[:, 0], data[:, 1], c = labels)
+plt.show()
+    
+labels_orig = labels
+#labels = np.mod(labels_orig, 2)
+
+plt.scatter(data[:, 0], data[:, 1], c = labels)
+plt.show()
+
+X_train, X_val, Y_train, Y_val = train_test_split(data, labels, stratify = labels_orig, random_state = 0)
+print(X_train.shape, X_val.shape)
+print(Y_train.shape)
+
+enc = OneHotEncoder()
+y_OH_train = enc.fit_transform(np.expand_dims(Y_train, 1)).toarray()
+y_OH_val = enc.fit_transform(np.expand_dims(Y_val, 1)).toarray()
 
 
+class FFSNMultiClassNetwork:
+    
+    def __init__(self, n_inputs, n_outputs, hidden_sizes=[2]):
+        self.nx = n_inputs
+        self.ny = n_outputs
+        self.nh = len(hidden_sizes)
+        self.sizes = [self.nx] + hidden_sizes + [self.ny]
+        
+        self.W = {}
+        self.B = {}
+        
+        for i in range(self.nh + 1):
+            self.W[i+1] = np.random.randn(self.sizes[i], self.sizes[i+1])
+            self.B[i+1] = np.zeros((1, self.sizes[i+1]))
+            
+    def sigmoid(self, x):
+        return 1.0/(1.0 + np.exp(-x))
+    
+    def forward_pass(self, x):
+        self.A = {}
+        self.H = {}
+        
+        self.H[0] = x.reshape(1, -1)
+        
+        for i in range(self.nh + 1):
+            self.A[i+1] = np.matmul(self.H[i], self.W[i+1]) + self.B[i+1]
+            self.H[i+1] = self.sigmoid(self.A[i+1])
+        return self.H[self.nh+1]
+    
+    def grad_sigmoid(self, x):
+        return x*(1-x)
+    
+    def grad(self, x, y):
+        self.forward_pass(x)
+        self.dW = {}
+        self.dB = {}
+        self.dH = {}
+        self.dA = {}
+        L = self.nh + 1
+        self.dA[L] = (self.H[L] - y)
+        for k in range(L, 0, -1):
+          self.dW[k] = np.matmul(self.H[k-1].T, self.dA[k])
+          self.dB[k] = self.dA[k]
+          self.dH[k-1] = np.matmul(self.dA[k], self.W[k].T)
+          self.dA[k-1] = np.multiply(self.dH[k-1], self.grad_sigmoid(self.H[k-1]))
+          
+    def fit(self, X, Y, epochs=1, learning_rate=1, initialize=True, display_loss=False):
+        
+        if initialize:
+            for i in range(self.nh + 1):
+                self.W[i+1] = np.random.randn(self.sizes[i], self.sizes[i+1])
+                self.B[i+1] = np.zeros((1, self.sizes[i+1])) 
+        
+        if display_loss:
+            loss = {}
+            
+        for e in trange(epochs):
+            dW = {}
+            dB = {}
+            for i in range(self.nh + 1):
+                dW[i+1] = np.zeros((self.sizes[i], self.sizes[i+1]))
+                dB[i+1] = np.zeros((1, self.sizes[i+1])) 
+            for x, y in zip(X, Y):
+                self.grad(x, y)
+                for i in range(self.nh + 1):
+                    dW[i+1] += self.dW[i+1]
+                    dB[i+1] += self.dB[i+1]
+            
+            m = X.shape[1]
+            for i in range(self.nh + 1):
+                self.W[i+1] -= learning_rate * dW[i+1] / m
+                self.B[i+1] -= learning_rate * dB[i+1] / m
+            
+            if display_loss:
+                Y_pred = self.predict(X)
+                loss[e] = mean_squared_error(Y_pred, Y)
+                
+        if display_loss:
+            plt.plot(loss.values())
+            plt.xlabel('Epochs')
+            plt.ylabel('Mean Squared Error')
+            plt.show()
+                
+    def predict(self, X):
+        Y_pred = []
+        for x in X:
+            y_pred = self.forward_pass(x)
+            Y_pred.append(y_pred)
+        return np.array(Y_pred).squeeze()
+                
+# FFNetwork Classification
+            
+ffsnn_multi = FFSNMultiClassNetwork(2, 4, [2, 3])
+ffsnn_multi.fit(X_train, y_OH_train, epochs=2000, learning_rate = 0.005, display_loss = True)
 
 
+# Train predict for see the accuracy
+
+Y_pred_train = ffsnn_multi.predict(X_train)
+Y_pred_train = np.argmax(Y_pred_train, 1)
+accuracy_train = accuracy_score(Y_pred_train, Y_train)
+print(round(accuracy_train, 2))
+
+# Predict validation part
+
+Y_pred_val = ffsnn_multi.predict(X_val)
+Y_pred_val =   np.argmax(Y_pred_val, 1)
+accuracy_val = accuracy_score(Y_pred_val, Y_val)
+print(round(accuracy_val, 2))
 
 
+# plot graph for seeing howmany are true predict
+
+plt.scatter(X_train[:, 0], X_train[:, 1], c = Y_pred_train, s = 15*(np.abs(Y_pred_train - Y_train) + 0.2))
 
 
 
